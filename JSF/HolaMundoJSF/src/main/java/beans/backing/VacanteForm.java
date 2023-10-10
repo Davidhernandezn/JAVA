57a9e198ffe829a665161911fe31857db73ba477
David_Remote <d4vidhernandezn@gmail.com>
2023-10-10 13:16:05 -0600

    Envio mensajes por log, Desarrollo estable

--- JSF/HolaMundoJSF/src/main/java/beans/backing/VacanteForm.java
+++ JSF/HolaMundoJSF/src/main/java/beans/backing/VacanteForm.java
@@ -4,6 +4,8 @@ import beans.model.Candidato;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**** @author dahernandez*/
 
@@ -13,6 +15,7 @@ public class VacanteForm {
     
     @Inject //INJECTAR DEPENDENCIA PARA poder USARLO
     private Candidato cadidato;
+    Logger log = LogManager.getRootLogger();//PARA PRUEBA DE LOG CICLO DE VIDA
 
     public void setCadidato(Candidato cadidato) {
         this.cadidato = cadidato; //para modificarlo
@@ -20,8 +23,10 @@ public class VacanteForm {
     
     public String enviar(){
     if(this.cadidato.getNombre().equals("Juan")){
+      log.info("ENTRANDO A CASO EXITO ");
     return "exito";
     }else{
+        log.info("ENTRANDO A CASO FALLO");
      return "fallo";   
     }
     
--- JSF/HolaMundoJSF/src/main/java/beans/model/Candidato.java
+++ JSF/HolaMundoJSF/src/main/java/beans/model/Candidato.java
@@ -1,6 +1,8 @@
 package beans.model;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 /**** @author dahernandez*/
 
 @Named//CLASE ASOCIADA AL HTML reconce al jsf CDI
@@ -9,6 +11,9 @@ public class Candidato {
     //Atributo 
     private String nombre = "Ingresa tu nombre";
     
+    //PARA PONER EN PRACTICA EL CILO DE VIDA
+    Logger log = LogManager.getRootLogger();
+    
     //GENERA getter y setter
     public String getNombre() {
         return nombre;
@@ -16,6 +21,8 @@ public class Candidato {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+        //Imprimimos por un Log veremos el momento
+        log.info("modificando propiedad nombre: "+this.nombre);
     }
     
 }
